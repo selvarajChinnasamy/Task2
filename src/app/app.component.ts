e086134cc6fcd90c0d1739b54e82b791e8b9df8b
selvarajChinnasamy <selvaraj1234597@gmail.com>
2017-12-01 16:25:54 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   template: `
+			   <post-banner></post-banner>	
+             `
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
 }
+    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent }  from './app.component';
+import { MyPostBannerComponent }  from './mypost-banner.component';
+import { ArticleComponent }  from './article.component';
+import { TechnologyComponent }  from './technology.component';
+import { MyPostComponent }  from './mypost.component';
+import { MyPostDirective }  from './mypost.directive';
+import { MyPostService }  from './mypost.service';
 
 @NgModule({
+  imports: [     
+        BrowserModule
+  ],
   declarations: [
-    AppComponent
+        AppComponent,
+		MyPostBannerComponent,
+		ArticleComponent,
+		TechnologyComponent,
+		MyPostComponent,
+		MyPostDirective		
   ],
-  imports: [
-    BrowserModule
+  entryComponents: [ 
+        ArticleComponent, 
+		TechnologyComponent 
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ 
+        MyPostService
+  ],  
+  bootstrap: [
+        AppComponent
+  ]
 })
 export class AppModule { }
